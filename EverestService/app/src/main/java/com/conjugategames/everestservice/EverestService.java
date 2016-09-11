f5f16518779bc8c9915559fc4b8bf35f9931ccc3
Steven Levis <levis501@yahoo.com>
2016-09-10 22:11:41 -0700

    EverestService gets data now that headphone firmware is updated.

--- EverestService/app/src/main/java/com/conjugategames/everestservice/EverestService.java
+++ EverestService/app/src/main/java/com/conjugategames/everestservice/EverestService.java
@@ -9,8 +9,12 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.harman.everestelite.ANCAwarenessPreset;
+import com.harman.everestelite.ANCCtrlListner;
 import com.harman.everestelite.Bluetooth;
 import com.harman.everestelite.BluetoothListener;
+import com.harman.everestelite.CommonListner;
+import com.harman.everestelite.HeadPhoneCtrl;
 
 import java.io.IOException;
 
@@ -27,6 +31,7 @@ public class EverestService extends Service implements BluetoothListener {
 
     private final Handler handler = new Handler();
     private Bluetooth connector;
+    private HeadPhoneCtrl headPhoneCtrl;
 
     private int numIntent;
 
@@ -69,10 +74,10 @@ public class EverestService extends Service implements BluetoothListener {
             e.printStackTrace();
         }
 
-        numIntent = 0;
-        // We first start the Handler
-        handler.removeCallbacks(sendData);
-        handler.postDelayed(sendData, 1000);
+//        numIntent = 0;
+//        // We first start the Handler
+//        handler.removeCallbacks(sendData);
+//        handler.postDelayed(sendData, 1000);
 
         return START_STICKY;
     }
@@ -99,6 +104,130 @@ public class EverestService extends Service implements BluetoothListener {
     @Override
     public void bluetoothDeviceConnected(Bluetooth bluetooth, BluetoothDevice bluetoothDevice, BluetoothSocket bluetoothSocket) {
         Log.d("EVEREST", "bluetoothDeviceConnected");
+        if (headPhoneCtrl != null && headPhoneCtrl.getSocket().equals(bluetoothSocket)) {
+            headPhoneCtrl.resetHeadPhoneCtrl(bluetoothSocket);
+        } else {
+            try {
+                headPhoneCtrl.close();
+                headPhoneCtrl = null;
+            } catch (Exception e) {
+            }
+            headPhoneCtrl = HeadPhoneCtrl.getInstance(MainActivity.activity, bluetoothSocket);
+//            headPhoneCtrl.ancCtrl.getBatteryLevel();
+            headPhoneCtrl.commonCtrl.set9AxisSensorStatus(true);
+            headPhoneCtrl.commonCtrl.set9AxisPushFrequency(200);
+            headPhoneCtrl.commonCtrl.get9AxisRawData();
+
+            headPhoneCtrl.setAncListner(new ANCCtrlListner() {
+                @Override
+                public void getANCSwitchStateReply(boolean b) {
+                    Log.d("EVEREST","getANCSwitchStateReply("+b+")");
+                }
+
+                @Override
+                public void getANCAwarenessPresetReply(ANCAwarenessPreset ancAwarenessPreset) {
+                    Log.d("EVEREST","getANCAwarenessPresetReply("+ancAwarenessPreset.toString()+")");
+                }
+
+                @Override
+                public void getLeftANCValueReply(long l) {
+                    Log.d("EVEREST","getLeftANCValueReply("+l+")");
+                }
+
+                @Override
+                public void getRightANCValueReply(long l) {
+                    Log.d("EVEREST","getRightANCValueReply("+l+")");
+                }
+
+                @Override
+                public void getBatteryLevelReply(long l) {
+                    Log.d("EVEREST","getBatteryLevelReply("+l+")");
+                }
+            });
+
+
+            headPhoneCtrl.setCommonListner(new CommonListner() {
+                @Override
+                public void getProgrammableIndexButtonReply(int i) {
+                    Log.d("EVEREST","getProgrammableIndexButtonReply("+i+")");
+                }
+
+                @Override
+                public void getConfigModelNumberReply(String s) {
+                    Log.d("EVEREST","getConfigModelNumberReply("+s+")");
+                }
+
+                @Override
+                public void getConfigProductNameReply(String s) {
+                    Log.d("EVEREST","getConfigProductNameReply("+s+")");
+                }
+
+                @Override
+                public void getAutoOffFeatureReply(boolean b) {
+                    Log.d("EVEREST","getAutoOffFeatureReply("+b+")");
+                }
+
+                @Override
+                public void getEnableVoicePromptReply(boolean b) {
+                    Log.d("EVEREST","getEnableVoicePromptReply("+b+")");
+                }
+
+                @Override
+                public void getFirmwareVersionReply(int i, int i1, int i2) {
+                    Log.d("EVEREST","getFirmwareVersionReply("+i+","+i1+","+i2+")");
+                }
+
+                @Override
+                public void waitCommandReplyElapsedTime(int i) {
+                    Log.d("EVEREST","waitCommandReplyElapsedTime("+i+")");
+                }
+
+                @Override
+                public void headPhoneError(Exception e) {
+                    Log.d("EVEREST","headPhoneError("+e.toString()+")");
+                }
+
+                @Override
+                public void setAutoOffFeatureReply(boolean b) {
+                    Log.d("EVEREST","setAutoOffFeatureReply("+b+")");
+                }
+
+                @Override
+                public void setEnableVoicePromptReply(boolean b) {
+                    Log.d("EVEREST","setEnableVoicePromptReply("+b+")");
+                }
+
+                @Override
+                public void getCustomButtonReply() {
+                    Log.d("EVEREST","getCustomButtonReply()");
+                }
+
+                @Override
+                public void get9AxisRawDataReply(double v, double v1, double v2, double v3, double v4, double v5, double v6, double v7, double v8) {
+                    Log.d("EVEREST", "get9AxisRawDataReply(" + v + "," + v1 + "," + v2 + "," + v3 + "," + v4 + "," + v5 + "," + v6 + "," + v7 + "," + v8);
+                }
+
+                @Override
+                public void get9AxisSensorStatusReply(boolean b) {
+                    Log.d("EVEREST","get9AxisSensorStatusReply("+b+")");
+                }
+
+                @Override
+                public void get9AxisPushFrequencyReply(int i) {
+                    Log.d("EVEREST","get9AxisPushFrequencyReply("+i+")");
+                }
+
+                @Override
+                public void set9AxisSensorStatusReply(boolean b) {
+                    Log.d("EVEREST","set9AxisSensorStatusReply("+b+")");
+                }
+
+                @Override
+                public void set9AxisPushFrequencyReply(boolean b) {
+                    Log.d("EVEREST","set9AxisPushFrequencyReply("+b+")");
+                }
+            });
+        }
     }
 
     @Override
---
+++ SonAR-Unity/SonAR/Assets/Scripts/Headphones.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class Headphones : MonoBehaviour {
+
+//  [DllImport ("everest-elite-sdk")]
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
